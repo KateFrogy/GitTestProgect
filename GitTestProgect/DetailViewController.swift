74518327f667bb1b5b4fc8ecbb28a265bedf8313
Ekaterina Isaeva <ek.aterinaIsaeva@mail.ru>
2025-05-13 22:26:53 +0300

    addaed smth

--- GitTestProgect/DetailViewController.swift
+++ GitTestProgect/DetailViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
     override func viewDidLoad() {
         super.viewDidLoad()
 
